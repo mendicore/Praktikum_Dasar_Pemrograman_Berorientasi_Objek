a9b54f2f2f062d5a169f2c42468107d651683d17
Syarif <134999646+mendicore@users.noreply.github.com>
2024-11-02 22:13:00 +0700

    Create Television.java

---
+++ Peralatan/Television.java
@@ -0,0 +1,8 @@
+public class Television extends Appliance{
+    private int size;
+    
+    public Television(String product, String place, int size) {
+        super(product, place);
+        this.size = size;
+    }
+}
