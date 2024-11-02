3da7f640c151194efb2aaba346ee4b707382a666
Syarif <134999646+mendicore@users.noreply.github.com>
2024-11-02 22:13:30 +0700

    Create Audio.java

---
+++ Peralatan/Audio.java
@@ -0,0 +1,9 @@
+public class Audio extends Appliance {
+    private String color;
+
+    public Audio(String product, String place, String color){
+        super(product, place);
+        this.color = color;
+    }
+}
+   
