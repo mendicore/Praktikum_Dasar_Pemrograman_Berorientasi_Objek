2a9c3fb01358d6212f784f886d988f84389a3cd5
Syarif <134999646+mendicore@users.noreply.github.com>
2024-11-05 19:37:58 +0700

    Create Truck.java

---
+++ Sistem_Manajemen_Kendaraan_di_Perusahaan_Logistik/Truck.java
@@ -0,0 +1,15 @@
+public class Truck extends Vehicle{
+    private double fuelConsumption;
+    private double distance;
+
+    public Truck(String plateNumber, double capacity, double fuelConsumption, double distance) {
+        super(plateNumber, capacity);
+        this.fuelConsumption = fuelConsumption;
+        this.distance = distance;
+    }
+    
+    @Override
+    public double calculateOperationalCost(){
+        return  fuelConsumption * distance * 1.5;
+    }
+}
